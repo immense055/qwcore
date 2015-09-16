4387387bbd5ff44cf0a796b6fe5ae1785fd854a1
Marcus Smith <qwcode@gmail.com>
2015-09-15 17:45:15 -0700

    don't wrap `configobj.ConfigObj`

--- qwcore/config.py
+++ qwcore/config.py
@@ -3,37 +3,26 @@ import os
 from appdirs import AppDirs
 import configobj
 
-from qwcore import exceptions
+from qwcore.exceptions import ConfigFileNotFoundError, ConfigFileParserError
 
 
 def get_config(appname):
-    """Return the config object for an app
+    """Return the `configobj` object for an app, assuming it's config file is in the
+    XDG compliant path for an app named `appname`, and where the actual config
+    file is named 'config'
 
     :param appname: app name
+    :raise ConfigFileNotFoundError: if the config file is not found
+    :raise ConfigFileParserError: if the config file is invalid
+
     """
     config_dir = AppDirs(appname).user_config_dir
     config_file = os.path.join(config_dir, 'config')
     if not os.path.isfile(config_file):
         msg = "Config file not found: {f}".format(f=config_file)
-        raise exceptions.ConfigFileNotFoundError(msg)
+        raise ConfigFileNotFoundError(msg)
     try:
         config = configobj.ConfigObj(config_file)
     except configobj.ConfigObjError as e:
-        raise exceptions.ConfigFileParserError(str(e))
-    return Config(config)
-
-
-class Config(object):
-
-    def __init__(self, configobj):
-        self.configobj = configobj
-
-    def get(self, key, section=None):
-        """Return a config value by key and section"""
-        config = self.configobj
-        try:
-            if section:
-                config = config[section]
-            return config[key]
-        except (KeyError, configobj.ConfigObjError) as e:
-            raise exceptions.ConfigFileKeyError(str(e))
+        raise ConfigFileParserError(str(e))
+    return config
--- qwcore/tests/test_config.py
+++ qwcore/tests/test_config.py
@@ -22,11 +22,3 @@ def test_get_config(tmpdir, monkeypatch):
     app_config_dir.join('config').write('key = value')
     monkeypatch.setattr('qwcore.config.AppDirs', lambda name: stub(user_config_dir=str(app_config_dir)))
     assert config.get_config('app').get('key') == 'value'
-
-
-def test_get_config_key_error(tmpdir, monkeypatch):
-    app_config_dir = tmpdir.mkdir('config')
-    app_config_dir.join('config').write('key = value')
-    monkeypatch.setattr('qwcore.config.AppDirs', lambda name: stub(user_config_dir=str(app_config_dir)))
-    with pytest.raises(exceptions.ConfigFileKeyError):
-        config.get_config('app').get('wrong_key')
